94022a178d296b22821253652aaa2193af12b9ee
foeye13579-source <foeye13579@gmail.com>
2026-01-01 21:36:53 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || 'f467669d-1b7c-405d-8150-709a0e6f3663'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
